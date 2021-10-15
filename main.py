832fc53c319d4b4b00d18231983024f2c0f5b326
AliciaD2018 <38516078+AliciaD2018@users.noreply.github.com>
2021-10-14 21:25:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import time
-
+import datetime
 import cv2
 import numpy as np
-import threading
-from multiprocessing import Process
 import os
 
-path = 'C:/Users/alicia/Documents/Semestre II 2021/Principios de Sistemas Operativos/yoloImage/'
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
-def detectar(video):
+from concurrent.futures import ProcessPoolExecutor
+
+pathSave1 = os.getcwd()
+path1 = pathSave1 + '/Imagenes1/'
+path2 = pathSave1 + '/Imagenes2/'
+path3 = pathSave1 + '/Imagenes3/'
+
+
+def detectar(video, path, nombre):
+    tiempo_final = 0
+    nombreVideo = str(nombre)
+    file1 = open(nombreVideo+'.txt',"w")
+    cantidad_pistolas = 0
+    cantidad_rifles = 0
+    cantidad_fuego = 0
     start_time = time.time()
     cap = cv2.VideoCapture(video)
     whT = 320
@@ -21,14 +35,17 @@ def detectar(video):
     with open(classesFile, 'rt') as f:
         classNames = f.read().rstrip('\n').split('\n')
 
-    modelConfiguration = 'config/yolov3.cfg'
-    modelWeights = 'config/yolov3.weights'
+    modelConfiguration = 'yolov3.cfg'
+    modelWeights = 'yolov3.weights'
 
     net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
     net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
     net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
     lista = []
+    listP = []
+    listR = []
+    listF = []
     def findObjects(outputs, img):
         hT, wT, cT = img.shape
         bbox = []
@@ -61,10 +78,46 @@ def detectar(video):
                 tiempo = round(time_milli / 1000)
                 if tiempo not in lista:
                     nombre = classNames[classIds[i]]
-                    cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i] * 100)}%',
+
+                    cv2.putText(img, f'{nombre.upper()} {int(confs[i] * 100)}%',
                                 (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
 
+
+
         return tiempo, nombre
+
+    def graficarDatos(nombre, tiempoFinal, cantidadPistolas, cantidadRifles, cantidadFuego):
+        dura=round(tiempoFinal)#Obtiene solo la parte entera
+        print("-------------------------------")
+        print("Nombre: " + nombre)
+        print("Tiempo: " + str(datetime.timedelta(seconds=dura)))
+        print("Pistolas: " + str(cantidadPistolas))
+        print("Rifles: " + str(cantidadRifles))
+        print("Fuego: " + str(cantidadFuego))
+        print("-------------------------------")
+
+        file1.write(str(cantidadPistolas)+'\n')
+        file1.write(str(cantidadRifles) + '\n')
+        file1.write(str(cantidadFuego) + '\n')
+        file1.close()
+
+        pathSave = os.getcwd()
+        fig = plt.figure(u'Gráfica de barras')  # Figure
+        ax = fig.add_subplot(111)  # Axes coordenada dentro de la ventana.
+
+        nombres = ['Pistolas', 'Rifles', 'Fuego']
+        datos = [cantidadPistolas, cantidadRifles, cantidadFuego]
+        colores = ["blue","red","green"]
+        xx = range(len(datos))
+
+        ax.bar(xx, datos,color=colores, width=0.5, align='center')
+        ax.set_xticks(xx)
+        ax.set_xticklabels(nombres)
+        ax.grid(axis='y', color='gray', linestyle='dashed')
+        plt.title('Cantidad de aparición ' + nombreVideo)
+        plt.savefig(pathSave+'/Graficas/'+nombre+'CantidadApariciones.png')
+
+        #plt.show()
 
     aux = 1
     contador = 0
@@ -83,28 +136,118 @@ def detectar(video):
                 tiempo, nombre = findObjects(outputs, img)
                 if nombre != "":
                     if tiempo not in lista:
+                        if nombre == "Rifle":
+                            cantidad_rifles = cantidad_rifles + 1
+                            listR.append(tiempo)
+                        elif nombre == "Gun":
+                            cantidad_pistolas = cantidad_pistolas + 1
+                            listP.append(tiempo)
+                        elif nombre == "Fire":
+                            cantidad_fuego = cantidad_fuego + 1
+                            listF.append(tiempo)
                         lista.append(tiempo)
+
                         cv2.imwrite(path + str(nombre) + str(tiempo) + '.png', img)
 
                 cv2.imshow('Image', img)
                 cv2.waitKey(1)
             else:
-                print("--- %s seconds ---" % (time.time() - start_time))
+                tiempo_final = (time.time() - start_time)
+                fig, ax = plt.subplots()
+                temperaturas2 = dict(Pistolas=listP, Rifles=listR, Fuegos=listF)
+
+                ax.plot(temperaturas2['Pistolas'],marker = 'o', label='Pistolas')
+                ax.plot(temperaturas2['Rifles'],marker = 'o', label='Rifles')
+                ax.plot(temperaturas2['Fuegos'],marker = 'o', label='Fuegos')
+                ax.legend(loc='upper right')
+                ax.grid(axis='y', color='gray', linestyle='dashed')
+                pathSave = os.getcwd()
+                plt.title('Tiempos de aparición '+ nombreVideo)
+                plt.savefig(pathSave+'/Graficas/' + nombreVideo + 'TiemposAparición.png')
+                #plt.show()
+                graficarDatos(nombreVideo, tiempo_final, cantidad_pistolas, cantidad_rifles, cantidad_fuego)
                 break
             aux+=1
         else:
             aux+=1
+    return cantidad_pistolas, cantidad_rifles, cantidad_fuego, tiempo_final
 
 if __name__ == '__main__':
-    cores=os.cpu_count()
-    print('Tengo ',cores, ' cores')
-    proceso=Process(target=detectar, args=('3.mp4',))
-    proceso2=Process(target=detectar,args=('Caceria.mp4',))
-    proceso.start()
-    proceso2.start()
-    proceso.join()
+    process1 = []
+    process2 = []
+    with ProcessPoolExecutor(max_workers=3) as executor:
+        executor.submit(detectar, '3.mp4', path1, 'Video1')
+        executor.submit(detectar, '5.mp4', path2, 'Video2')
+        executor.submit(detectar, '6.mp4', path3, 'Video3')
 
-    #thread = threading.Thread(target=detectar, args=('3.mp4',))
-    #thread.start()
+#     p1 = 0
+#     r1 = 0
+#     f1 = 0
+#
+#     p2 = 0
+#     r2 = 0
+#     f2 = 0
+#
+#     p3 = 0
+#     r3 = 0
+#     f3 = 0
+#
+#     v1 = open('Video1.txt','r')
+#     cont = 1
+#     for line in v1:
+#         if cont == 1:
+#             p1 = int(line)
+#             cont = cont + 1
+#         elif cont == 2:
+#             r1 = int(line)
+#             cont = cont + 1
+#         else:
+#             f1 = int(line)
+#             cont = 1
+#     v2 = open('Video2.txt','r')
+#     for line in v2:
+#         if cont == 1:
+#             p2 = int(line)
+#             cont = cont + 1
+#         elif cont == 2:
+#             r2 = int(line)
+#             cont = cont + 1
+#         else:
+#             f2 = int(line)
+#             cont = 1
+#     v3 = open('Video3.txt', 'r')
+#     for line in v3:
+#         if cont == 1:
+#             p3 = int(line)
+#             cont = cont + 1
+#         elif cont == 2:
+#             r3 = int(line)
+#             cont = cont + 1
+#         else:
+#             f3 = int(line)
+#             cont = 1
+#
+#     pTotal = p1 + p2 + p3
+#     rTotal = r1 + r2 + p3
+#     fTotal = f1 + f2 + p3
+#
+#
+#     pathSave = os.getcwd()
+#     print(pathSave)
+#     fig = plt.figure(u'Clases Sumadas')  # Figure
+#     ax = fig.add_subplot(111)  # Axes coordenada dentro de la ventana.
+#
+#     nombres = ['Pistolas', 'Rifles', 'Fuego']
+#     datos = [pTotal, rTotal, fTotal]
+#     xx = range(len(datos))
+#     colores = ["blue", "red", "green"]
+#     ax.bar(xx, datos, color=colores, width=0.5, align='center')
+#     ax.set_xticks(xx)
+#     ax.set_xticklabels(nombres)
+#     ax.grid(axis='y', color='gray', linestyle='dashed')
+#     plt.title("Datos Generales")
+#     plt.savefig(pathSave+'\Graficas\ApariciónGeneralObjetos.png')
+#
+#
+# #------------------------------------------------------------------------
 
-#------------------------------------------------------------------------
